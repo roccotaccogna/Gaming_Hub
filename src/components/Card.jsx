8e0958aef34b2af776a806b481835407c6fcafc8
Rocco Taccogna <roccotaccogna@hotmail.com>
2023-12-14 12:19:21 +0100

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ export default function Card({game}){
     <figure><img src={game.background_image} alt="game image"/></figure>
     <div className="card-body">
       <h4  className="card-title text-white">{game.name}</h4>  
-        <p className="text-sm">{game.genres.map((genre) => genre.name).join(', ')}</p>
+        <p className="text-sm text-slate-400">{game.genres.map((genre) => genre.name).join(', ')}</p>
         <div className="card-action justify-center mt-2">
           <Link to={`/game/${game.slug}`} className="btn btn-neutral uppercase w-24">
             Details
