2a32ab08966acd28cc3cc47fa8dd074d1b752153
Rocco Taccogna <roccotaccogna@hotmail.com>
2023-12-18 16:54:30 +0100

    update-navbar

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -5,7 +5,7 @@ import AppContext from "../contexts/AppContext";
 import supabase from "../supabase/client";
 import { MdOutlineAccountCircle } from "react-icons/md";
 import { CiSettings } from "react-icons/ci";
-import { IoIosArrowDown } from "react-icons/io";
+import { IoMdExit } from "react-icons/io";
 import "../styles/dropdownNav.css";
 
 export default function AppNavbar(){
@@ -29,33 +29,51 @@ export default function AppNavbar(){
           </Link>
 
           {session ? (
-            <div className='dropdown-content mr-6'>
-              <div className="dropdown">
-                <div tabIndex={0} className="btn btn-ghost m-1 text-lg text-white bg-transparent border-none no-underline" role="button">
-                  {session.user.user_metadata.username || session.user.email}           
-                  <IoIosArrowDown className="bg-transparent"/>
-                </div> 
-                 <ul className="dropdown-content p-2 w-60 text-white">
-                    <li className="text-lg text-gray-400">
-                      <Link to={{ pathname:"/account" }} className="hover:text-white flex">
-                        <MdOutlineAccountCircle size={26} className="ml-1"/>
-                        Account Page
-                      </Link>
-                    </li>
-                    <li className="text-lg text-gray-400">
-                      <Link to={{ pathname:"/settings" }} className="hover:text-white flex mt-2">
-                        <CiSettings size={26} className="ml-1"/>
-                        Settings Page
-                      </Link>
-                    </li>
-                    <button onClick={handleSignOut} 
-                        className="cursor-pointer mt-2 ml-6 hover:text-red-500 uppercase text-lg" >
-                          Sign out
-                    </button>
-                  </ul> 
-              </div>
-            </div>
+            // <div className='dropdown-content mr-6'>
+            //   <div className="dropdown">
+            //     <div tabIndex={0} className="btn btn-ghost m-1 text-lg text-white bg-transparent border-none no-underline" role="button">
+            //       {session.user.user_metadata.username || session.user.email}           
+            //       <IoIosArrowDown className="bg-transparent"/>
+            //     </div> 
+            //      <ul className="dropdown-content p-2 w-60 text-white">
+            //         <li className="text-lg text-gray-400">
+            //           <Link to="/account" className="hover:text-white flex">
+            //             <MdOutlineAccountCircle size={26} className="ml-1"/>
+            //             Account Page
+            //           </Link>
+            //         </li>
+            //         <li className="text-lg text-gray-400">
+            //           <Link to="/settings" className="hover:text-white flex mt-2">
+            //             <CiSettings size={26} className="ml-1"/>
+            //             Settings Page
+            //           </Link>
+            //         </li>
+            //         <button onClick={handleSignOut} 
+            //             className="cursor-pointer mt-2 ml-6 hover:text-red-500 uppercase text-lg" >
+            //               Sign out
+            //         </button>
+            //       </ul> 
+            //   </div>
+            // </div>
+
+            <ul className="menu-horizontal">
+            <li className="mr-8">
+                <Link to="/account" className="hover:text-white">
+                  <MdOutlineAccountCircle size={26}/>
+                </Link>
+            </li>
+            <li>
+              <Link to="/settings" className="hover:text-white">
+                <CiSettings size={26}/>
+              </Link>
+            </li>
+            <button onClick={handleSignOut} 
+              className="cursor-pointer ml-8 hover:text-red-500 uppercase text-sm" >
+                <IoMdExit size={26}/>
+            </button>
+          </ul>
           ): (
+            
               <ul className="menu-horizontal">
                 <li className="mr-8"><Link to="/login" className="uppercase hover:text-white text-gray-400 text-sm">Login</Link></li>
                 <li><Link to="/register" className="uppercase hover:text-white text-gray-400 text-sm">Register</Link></li>
